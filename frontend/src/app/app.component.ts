f7e9f08fb3925c76080fc1291b12d0a8f73cffd2
jvanheesch <joris.vanheesch@vdab.be>
2022-06-17 15:25:04 +0200

    upload

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+    loading = false;
+
+    constructor(
+        private http: HttpClient
+    ) {
+    }
+
+    fileChange(event: any) {
+        let fileList: FileList = event.target.files;
+        if (fileList.length > 0) {
+            this.loading = true;
+            let file: File = fileList[0];
+            let formData: FormData = new FormData();
+            formData.append('file', file, file.name);
+            this.http
+                .post('/documents', formData)
+                .subscribe(() => this.loading = false);
+        }
+    }
 }
