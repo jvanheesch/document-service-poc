8d512010377f59e048dfca72b84063e05ef803ab
jvanheesch <joris.vanheesch@vdab.be>
2022-06-17 16:35:41 +0200

    add label displaying document status in f/e

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, NgZone} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
     selector: 'app-root',
@@ -8,13 +9,15 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AppComponent {
     loading = false;
+    status = new BehaviorSubject<string>('');
 
     constructor(
-        private http: HttpClient
+        private http: HttpClient,
+        private _ngZone: NgZone,
     ) {
     }
 
-    fileChange(event: any) {
+    async fileChange(event: any) {
         let fileList: FileList = event.target.files;
         if (fileList.length > 0) {
             this.loading = true;
@@ -23,7 +26,13 @@ export class AppComponent {
             formData.append('file', file, file.name);
             this.http
                 .post('/documents', formData)
-                .subscribe(() => this.loading = false);
+                .subscribe((document: any) => {
+                    this.loading = false;
+
+                    const source = new EventSource(`/documents/${document.id}/status`); // jshint ignore:line
+                    source.onerror = () => source.close();
+                    source.onmessage = (event) => this._ngZone.run(() => this.status.next(event.data.replaceAll('\"', '')));
+                });
         }
     }
 }
